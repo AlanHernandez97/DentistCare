29c8e5d2f3e9827b835cccdbdfe5c9d214691aac
Alan Lopez <yondaime_97@live.com>
2024-07-16 23:38:40 -0600

    Navbar funcionando

--- app/(views)/layout.tsx
+++ app/(views)/layout.tsx
@@ -1,9 +1,7 @@
-import Navbar from "@/components/navbar/Navbar";
 
 export default function PageLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<>
-			<Navbar />
 			<div className="flex flex-col items-center p-24">
 				<h1 className="text-2xl">Hola Mundo</h1>
 				{children}
--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -3,14 +3,14 @@ import React from 'react'
 
 function Navbar() {
 	return (
-		<nav className='flex justify-between p-10 bg-[#299FF4] text-white items-center'>
+		<nav className='flex justify-between p-8 bg-[#299FF4] text-white items-center'>
 			<span className='text-2xl'>DentistCare</span>
 			<div className='flex gap-20 items-center'>
-				<Link className='hover:underline' href={'#'}>Inicio</Link>
-				<Link className='hover:underline' href={'#'}>Precios</Link>
-				<Link className='hover:underline' href={'#'}>Nosotros</Link>
-				<Link className='hover:underline' href={'#'}>Servicios</Link>
-				<Link className='border p-3 hover:underline rounded-md' href={'#'}>Contáctanos</Link>
+				<Link className='hover:underline' href={'/'}>Inicio</Link>
+				<Link className='hover:underline' href={'/pricing'}>Precios</Link>
+				<Link className='hover:underline' href={'/about'}>Nosotros</Link>
+				<Link className='hover:underline' href={'/service'}>Servicios</Link>
+				<Link className='border p-3 hover:underline rounded-md' href={'/contact'}>Contáctanos</Link>
 			</div>
 		</nav>
 	)
