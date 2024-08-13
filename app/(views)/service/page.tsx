3b35cb40ba6ac7b927c2ef1fde91e62ff3843ff4
Alan Lopez <yondaime_97@live.com>
2024-08-13 00:27:17 -0600

    Landing Finished

--- app/(views)/service/page.tsx
+++ app/(views)/service/page.tsx
@@ -2,53 +2,13 @@ import ServiceCard from '@/src/components/Atoms/ServiceCard'
 import Steps from '@/src/components/Organisms/steps/Steps'
 import type { Metadata } from 'next'
 
+
 export const metadata: Metadata = {
 	title: 'Servicios',
 	description: 'Estos son los servicios que manejamos'
 }
 
 export default function ServicePage() {
-
-	const DentistServices = [
-		{
-			id: 1,
-			serviceTitle: "Exámenes de laboratorio",
-			services: ["Biometría Hemática", "Quimica Sanguinea", "Examen Histopatológico"],
-			prices: [400, 500, 800]
-		},
-		{
-			id: 2,
-			serviceTitle: "Radiografías Dentales",
-			services: ["Radiografía Periapical", "Aleta De Mordida", "Ortopantomografía"],
-			prices: [400, 700, 600]
-		},
-		{
-			id: 3,
-			serviceTitle: "Cirugías Dentales",
-			services: ["Injerto de encía", "Cirugía Maxilofacial", "Implantes Dentales"],
-			prices: [1200, 6000, 3400]
-		},
-		{
-			id: 4,
-			serviceTitle: "Consultas",
-			services: ["Consulta General"],
-			prices: [800]
-		},
-		{
-			id: 5,
-			serviceTitle: "Cirugías Maxilofaciales",
-			services: ["Cirugía Ortonágtica", "Cirugía Mandibular Correctiva", "Exodoncia", "Implantología"],
-			prices: [1200, 6000, 3400, 4700]
-		},
-		{
-			id: 6,
-			serviceTitle: "Otros Servicios",
-			services: ["Ortodoncia", "Endodoncia", "Limpieza Dental", "Odontología Pediátrica"],
-			prices: [1200, 6000, 3400, 4700, 1000]
-		},
-
-
-	];
 
 
 	return (
@@ -67,18 +27,7 @@ export default function ServicePage() {
 					<h3 className="text-4xl font-bold text-center">Conoce nuestros servicios</h3>
 				</div>
 				<div className='flex flex-wrap justify-center gap-5 py-10 px-5'>
-					{
-						DentistServices.map(services => (
-
-							<ServiceCard
-								key={services.id}
-								serviceTitle={services.serviceTitle}
-								services={services.services[0]}
-								price={services.prices[0]}
-							/>
-
-						))
-					}
+					<ServiceCard />
 				</div>
 			</div>
 		</div>
--- src/components/Atoms/ServiceCard.tsx
+++ src/components/Atoms/ServiceCard.tsx
@@ -1,26 +1,86 @@
 
 
 type ServiceCardProps = {
+	id: number;
 	serviceTitle?: string;
-	services?: string[] | string;
-	price?: number | number[];
+	services?: string[];
+	prices?: number[];
 }
 
-export default function ServiceCard({ price, serviceTitle, services }: ServiceCardProps) {
-	return (
-		<div
-			className='w-full h-fit shadow-sm bg-[#F8F8F8] px-5 py-7 rounded-md hover:shadow-2xl hover:shadow-black hover:-translate-y-4 transition ease-in-out duration-500 md:w-96'
-		>
+export default function ServiceCard() {
+	const DentistServices: ServiceCardProps[] = [
+		{
+			id: 1,
+			serviceTitle: "Exámenes de laboratorio",
+			services: ["Biometría Hemática", "Quimica Sanguinea", "Examen Histopatológico"],
+			prices: [400, 500, 800]
+		},
+		{
+			id: 2,
+			serviceTitle: "Radiografías Dentales",
+			services: ["Radiografía Periapical", "Aleta De Mordida", "Ortopantomografía"],
+			prices: [400, 700, 600]
+		},
+		{
+			id: 3,
+			serviceTitle: "Cirugías Dentales",
+			services: ["Injerto de encía", "Cirugía Maxilofacial", "Implantes Dentales"],
+			prices: [1200, 6000, 3400]
+		},
+		{
+			id: 4,
+			serviceTitle: "Consultas",
+			services: ["Consulta General"],
+			prices: [800]
+		},
+		{
+			id: 5,
+			serviceTitle: "Cirugías Maxilofaciales",
+			services: ["Cirugía Ortonágtica", "Cirugía Mandibular Correctiva", "Exodoncia", "Implantología"],
+			prices: [1200, 6000, 3400, 4700]
+		},
+		{
+			id: 6,
+			serviceTitle: "Otros Servicios",
+			services: ["Ortodoncia", "Endodoncia", "Limpieza Dental", "Odontología Pediátrica"],
+			prices: [1200, 6000, 3400, 4700, 1000]
+		},
 
-			<div className="flex flex-col gap-3">
-				<span className="text-2xl font-bold text-center">
-					{serviceTitle}
-				</span>
-				<div className="flex justify-between items-center">
-					<li>{services}</li>
-					<span>${price} MXN</span>
-				</div>
-			</div>
-		</div>
+
+	];
+	return (
+		<>
+			{
+				DentistServices.map(services => (
+					<div
+						className='w-full h-fit shadow-sm bg-[#F8F8F8] px-5 py-7 rounded-md hover:shadow-2xl hover:shadow-black hover:-translate-y-4 transition ease-in-out duration-500 md:w-96'
+					>
+
+						<div className="flex flex-col gap-3">
+							<span className="text-2xl font-bold text-center">
+								{services.serviceTitle}
+							</span>
+							<div className="flex justify-between items-center">
+								<div className="flex flex-col">
+									{
+										services.services?.map(service => (
+											<li>{service}</li>
+										))
+									}
+								</div>
+								<div className="flex flex-col">
+									{
+										services.prices?.map(price => (
+											<span>{price} MXN</span>
+										))
+									}
+								</div>
+
+							</div>
+						</div>
+					</div>
+				))
+			}
+		</>
 	)
 }
